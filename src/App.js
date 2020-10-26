4f4baa0ac19069c8ec684ac6f645fce8a69d1462
Zeljka <zeljka_raic@yahoo.com>
2020-10-26 21:29:20 +0100

    Updating App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, {Component} from "react";
 import ListOfMessages from "./Components/ListOfMessages";
 import './App.css';
+import { randomColor, randomName } from "./Components/UserName";
 
-function App() {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [],
+      member: {
+        username: randomName(),
+        color: randomColor()
+      }
+    }
+  }
+  
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
+
+        <ListOfMessages
+        messages={this.state.messages}
+        currentMember={this.state.member}/>
         
-      </header>
+      </div>
     </div>
   );
 }
 
-export default App;
+}
