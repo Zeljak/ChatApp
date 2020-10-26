cfff0f7263ae324063d24b076704ccb64448cf7e
Zeljka <zeljka_raic@yahoo.com>
2020-10-26 21:36:29 +0100

    Input Component

---
+++ src/Components/Input.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+export default class Input extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            text: "",
+        };
+    }
+
+    render(){
+        return(
+            <div className="Input">
+                
+            </div>
+        )
+    }
+}
+
