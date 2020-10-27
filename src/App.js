ba608edec81e7a8d5622ac724b605aa8a16164e8
Zeljka <zeljka_raic@yahoo.com>
2020-10-27 14:04:17 +0100

    CSS & Header Image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ListOfMessages from "./Components/ListOfMessages";
 import './App.css';
 import { randomColor, randomName } from "./Components/UserName";
 import Input from "./Components/Input";
+import Image from "./Components/image_talking.jpg"
 
 export default class App extends Component {
   constructor(props) {
@@ -15,16 +16,26 @@ export default class App extends Component {
         color: randomColor()
       }
     }
+
+
+
+
+    
   }
+
   
   render(){
   return (
     <div className="App">
       <div className="App-header">
+        <img className="image" src={Image} alt="chat logo"/>
 
         <ListOfMessages
         messages={this.state.messages}
         currentMember={this.state.member}/>
+
+        <Input
+        onSendMessage={this.onSendMessage}/>
         
       </div>
     </div>
